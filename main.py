22e92b9681f2fc6fe871161c8ceae937b24ae664
Florian Gianni <florian.gianni@etu.imt-lille-douai.fr>
2020-11-03 13:53:54 +0100

    ajout de library

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from scipy.cluster import hierarchy
+from sklearn.mixture import GaussianMixture
+from sklearn.cluster import DBSCAN
+
