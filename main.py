4a2a7c152303f28272422b7ac098acd18a94e8e5
Florian Gianni <florian.gianni@etu.imt-lille-douai.fr>
2020-11-03 14:57:00 +0100

    importation et visualisation des donnees

--- main.py
+++ main.py
@@ -6,3 +6,26 @@ from scipy.cluster import hierarchy
 from sklearn.mixture import GaussianMixture
 from sklearn.cluster import DBSCAN
 
+# Importation des données
+df1 = pd.read_csv('./g2-2-20.txt', sep='     ', header=None, engine='python')
+df2 = pd.read_csv('./g2-2-100.txt', sep='     ', header=None, engine='python')
+df3 = pd.read_csv('./jain.txt', sep='\t', header=None)
+df4 = pd.read_csv('./Aggregation.txt', sep='\t', header=None)
+df5 = pd.read_csv('./pathbased.txt', sep='\t', header=None)
+
+
+# Affichage des nuages de points
+df1.plot.scatter(x=0, y=1)
+plt.show()
+
+df2.plot.scatter(x=0, y=1)
+plt.show()
+
+df3.plot.scatter(x=0, y=1, c=2, colormap='plasma')
+plt.show()
+
+df4.plot.scatter(x=0, y=1, c=2, colormap='plasma')
+plt.show()
+
+df5.plot.scatter(x=0, y=1, c=2, colormap='plasma')
+plt.show()
