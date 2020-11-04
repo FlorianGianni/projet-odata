bb10758954749e3beee332015947d0d6f06cf6f1
Alain Tran <alain59370@gmail.com>
2020-11-04 18:01:14 +0100

    texte

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from sklearn.cluster import KMeans
 from scipy.cluster import hierarchy
 from sklearn.mixture import GaussianMixture
 from sklearn.cluster import DBSCAN
+from sklearn.preprocessing import StandardScaler
 
 ### Importation des données
 df1 = pd.read_csv('./data/g2-2-20.txt', sep='     ', header=None, engine='python')
@@ -61,3 +62,86 @@ kmeans = KMeans(n_clusters=3, random_state=0).fit(X)
 cluster = kmeans.predict(X)
 df5.plot.scatter(x=0, y=1, c=cluster, colormap='plasma')
 plt.show()
+
+### CHA
+X = df1.to_numpy()
+scaler = StandardScaler()
+scaler.fit(X)
+Z = scaler.transform(X)
+Z1 = hierarchy.linkage(Z,'ward')
+# dn = hierarchy.dendrogram(Z1) #Pour avoir le t coupure
+# plt.show()
+cluster = hierarchy.fcluster(Z1,t=20,criterion='distance')
+df1.plot.scatter(x=0,y=1,c=cluster,colormap='plasma')
+
+X = df2.to_numpy()
+scaler = StandardScaler()
+scaler.fit(X)
+Z = scaler.transform(X)
+Z1 = hierarchy.linkage(Z,'ward')
+# dn = hierarchy.dendrogram(Z1) #Pour avoir le t coupure
+# plt.show()
+cluster = hierarchy.fcluster(Z1,t=60,criterion='distance')
+df2.plot.scatter(x=0,y=1,c=cluster,colormap='plasma')
+
+X = df3.to_numpy()
+scaler = StandardScaler()
+scaler.fit(X)
+Z = scaler.transform(X)
+Z1 = hierarchy.linkage(Z,'ward')
+# dn = hierarchy.dendrogram(Z1) #Pour avoir le t coupure
+# plt.show()
+cluster = hierarchy.fcluster(Z1,t=25,criterion='distance')
+df3.plot.scatter(x=0,y=1,c=cluster,colormap='plasma')
+
+X = df4.to_numpy()
+scaler = StandardScaler()
+scaler.fit(X)
+Z = scaler.transform(X)
+Z1 = hierarchy.linkage(Z,'ward')
+# dn = hierarchy.dendrogram(Z1) #Pour avoir le t coupure
+# plt.show()
+cluster = hierarchy.fcluster(Z1,t=9,criterion='distance')
+df4.plot.scatter(x=0,y=1,c=cluster,colormap='plasma')
+
+X = df5.to_numpy()
+scaler = StandardScaler()
+scaler.fit(X)
+Z = scaler.transform(X)
+Z1 = hierarchy.linkage(Z,'ward')
+# dn = hierarchy.dendrogram(Z1) #Pour avoir le t coupure
+# plt.show()
+cluster = hierarchy.fcluster(Z1,t=20,criterion='distance')
+df5.plot.scatter(x=0,y=1,c=cluster,colormap='plasma')
+
+##Methode Gausienne
+X = df1.to_numpy()
+Gausienne = GaussianMixture(n_components=2, random_state=0).fit(X)
+cluster = Gausienne.predict(X)
+df1.plot.scatter(x=0, y=1, c=cluster, colormap='plasma')
+plt.show()
+
+X = df2.to_numpy()
+Gausienne = GaussianMixture(n_components=1, random_state=0).fit(X)
+cluster = Gausienne.predict(X)
+df2.plot.scatter(x=0, y=1, c=cluster, colormap='plasma')
+plt.show()
+
+X = df3.to_numpy()
+Gausienne = GaussianMixture(n_components=2, random_state=0).fit(X)
+cluster = Gausienne.predict(X)
+df3.plot.scatter(x=0, y=1, c=cluster, colormap='plasma')
+plt.show()
+
+X = df4.to_numpy()
+Gausienne = GaussianMixture(n_components=7, random_state=0).fit(X)
+cluster = Gausienne.predict(X)
+df4.plot.scatter(x=0, y=1, c=cluster, colormap='plasma')
+plt.show()
+
+X = df5.to_numpy()
+Gausienne = GaussianMixture(n_components=3, random_state=0).fit(X)
+cluster = Gausienne.predict(X)
+df5.plot.scatter(x=0, y=1, c=cluster, colormap='plasma')
+plt.show()
+
