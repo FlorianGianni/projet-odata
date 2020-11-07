ecc9fc3142b7f97bafa839b7c7787016497ddba7
Florian Gianni <florian.gianni@etu.imt-lille-douai.fr>
2020-11-07 17:50:44 +0100

    refactorisation

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from scipy.cluster import hierarchy
 from sklearn.mixture import GaussianMixture
 from sklearn.cluster import DBSCAN
 from sklearn.preprocessing import StandardScaler
+from sklearn import metrics
 
 try:
     os.makedirs('./results/etude_prealable/')
@@ -21,169 +22,98 @@ df3 = pd.read_csv('./data/jain.txt', sep='\t', header=None)
 df4 = pd.read_csv('./data/Aggregation.txt', sep='\t', header=None)
 df5 = pd.read_csv('./data/pathbased.txt', sep='\t', header=None)
 
+df = [df1, df2, df3, df4, df5]
+
 ####  2.Etude prealable : Comparaison des methodes de clustering sur des donnees simulees
 
 ### 2.3 Experimentation
 
 ### Affichage des nuages de points
-df1.plot.scatter(x=0, y=1)
-plt.savefig('./results/etude_prealable/raw-1.png')
 
-df2.plot.scatter(x=0, y=1)
-plt.savefig('./results/etude_prealable/raw-2.png')
+def afficher(df, i, c=None, colormap=None):
+    df.plot.scatter(x=0, y=1, c=c, colormap=colormap)
+    plt.savefig('./results/etude_prealable/raw-' + str(i) + '.png')
 
-df3.plot.scatter(x=0, y=1, c=2, colormap='plasma')
-plt.savefig('./results/etude_prealable/raw-3.png')
+afficher(df1, 1)
+afficher(df2, 2)
+afficher(df3, 3, c=2, colormap='plasma')
+afficher(df4, 4, c=2, colormap='plasma')
+afficher(df5, 5, c=2, colormap='plasma')
 
-df4.plot.scatter(x=0, y=1, c=2, colormap='plasma')
-plt.savefig('./results/etude_prealable/raw-4.png')
-
-df5.plot.scatter(x=0, y=1, c=2, colormap='plasma')
-plt.savefig('./results/etude_prealable/raw-5.png')
+plt.close(fig='all')
 
 
 ### K-means
-X = df1.to_numpy()
-kmeans = KMeans(n_clusters=2, random_state=0).fit(X)
-cluster = kmeans.predict(X)
-df1.plot.scatter(x=0, y=1, c=cluster, colormap='plasma')
-plt.savefig('./results/etude_prealable/kmeans-1.png')
 
-X = df2.to_numpy()
-kmeans = KMeans(n_clusters=1, random_state=0).fit(X)
-cluster = kmeans.predict(X)
-df2.plot.scatter(x=0, y=1, c=cluster, colormap='plasma')
-plt.savefig('./results/etude_prealable/kmeans-2.png')
+def kmeans(df, i, n_clusters):
+    X = df.to_numpy()
+    cluster = KMeans(n_clusters=n_clusters, random_state=0).fit_predict(X)
+    df.plot.scatter(x=0, y=1, c=cluster, colormap='plasma')
+    plt.savefig('./results/etude_prealable/kmeans-' + str(i) + '.png')
 
-X = df3.to_numpy()
-kmeans = KMeans(n_clusters=2, random_state=0).fit(X)
-cluster = kmeans.predict(X)
-df3.plot.scatter(x=0, y=1, c=cluster, colormap='plasma')
-plt.savefig('./results/etude_prealable/kmeans-3.png')
 
-X = df4.to_numpy()
-kmeans = KMeans(n_clusters=7, random_state=0).fit(X)
-cluster = kmeans.predict(X)
-df4.plot.scatter(x=0, y=1, c=cluster, colormap='plasma')
-plt.savefig('./results/etude_prealable/kmeans-4.png')
+kmeans(df1, 1, n_clusters=2)
+kmeans(df2, 2, n_clusters=1)
+kmeans(df3, 3, n_clusters=2)
+kmeans(df4, 4, n_clusters=7)
+kmeans(df5, 5, n_clusters=3)
 
-X = df5.to_numpy()
-kmeans = KMeans(n_clusters=3, random_state=0).fit(X)
-cluster = kmeans.predict(X)
-df5.plot.scatter(x=0, y=1, c=cluster, colormap='plasma')
-plt.savefig('./results/etude_prealable/kmeans-5.png')
+plt.close(fig='all')
 
 
 ### CHA
-X = df1.to_numpy()
-sc = StandardScaler()
-Z = sc.fit_transform(X)
-Z1 = hierarchy.linkage(Z,'ward')
-# dn = hierarchy.dendrogram(Z1) # Pour avoir le t coupure
-# plt.savefig('./results/etude_prealable/cha-dendrogram-1.png')
-cluster = hierarchy.fcluster(Z1,t=20,criterion='distance')
-df1.plot.scatter(x=0,y=1,c=cluster,colormap='plasma')
-plt.savefig('./results/etude_prealable/cha-1.png')
 
-X = df2.to_numpy()
-sc = StandardScaler()
-Z = sc.fit_transform(X)
-Z1 = hierarchy.linkage(Z,'ward')
-# dn = hierarchy.dendrogram(Z1) # Pour avoir le t coupure
-# plt.savefig('./results/etude_prealable/cha-dendrogram-2.png')
-cluster = hierarchy.fcluster(Z1,t=60,criterion='distance')
-df2.plot.scatter(x=0,y=1,c=cluster,colormap='plasma')
-plt.savefig('./results/etude_prealable/cha-2.png')
+def cha(df, i, method, metric, t):
+    X = df.to_numpy()
+    Z = StandardScaler().fit_transform(X)
+    Z1 = hierarchy.linkage(Z, method=method, metric=metric)
+    # dn = hierarchy.dendrogram(Z1) # Pour avoir le t coupure
+    # plt.savefig('./results/etude_prealable/cha-dendrogram-' + str(i) + '.png')
+    cluster = hierarchy.fcluster(Z1, t=t, criterion='distance')
+    df.plot.scatter(x=0, y=1, c=cluster, colormap='plasma')
+    plt.savefig('./results/etude_prealable/cha-' + str(i) + '.png')
 
-X = df3.to_numpy()
-sc = StandardScaler()
-Z = sc.fit_transform(X)
-Z1 = hierarchy.linkage(Z,'ward')
-# dn = hierarchy.dendrogram(Z1) # Pour avoir le t coupure
-# plt.savefig('./results/etude_prealable/cha-dendrogram-3.png')
-cluster = hierarchy.fcluster(Z1,t=25,criterion='distance')
-df3.plot.scatter(x=0,y=1,c=cluster,colormap='plasma')
-plt.savefig('./results/etude_prealable/cha-3.png')
 
-X = df4.to_numpy()
-sc = StandardScaler()
-Z = sc.fit_transform(X)
-Z1 = hierarchy.linkage(Z,'ward')
-# dn = hierarchy.dendrogram(Z1) # Pour avoir le t coupure
-# plt.savefig('./results/etude_prealable/cha-dendrogram-4.png')
-cluster = hierarchy.fcluster(Z1,t=9,criterion='distance')
-df4.plot.scatter(x=0,y=1,c=cluster,colormap='plasma')
-plt.savefig('./results/etude_prealable/cha-4.png')
+cha(df1, 1, method='ward', metric='euclidean', t=20)
+cha(df2, 2, method='ward', metric='euclidean', t=60)
+cha(df3, 3, method='ward', metric='euclidean', t=25)
+cha(df4, 4, method='ward', metric='euclidean', t=9)
+cha(df5, 5, method='ward', metric='euclidean', t=20)
 
-X = df5.to_numpy()
-sc = StandardScaler()
-Z = sc.fit_transform(X)
-Z1 = hierarchy.linkage(Z,'ward')
-# dn = hierarchy.dendrogram(Z1) # Pour avoir le t coupure
-# plt.savefig('./results/etude_prealable/cha-dendrogram-5.png')
-cluster = hierarchy.fcluster(Z1,t=20,criterion='distance')
-df5.plot.scatter(x=0,y=1,c=cluster,colormap='plasma')
-plt.savefig('./results/etude_prealable/cha-5.png')
+plt.close(fig='all')
 
 
 ### Methode Gausienne
-X = df1.to_numpy()
-Gausienne = GaussianMixture(n_components=2, random_state=0).fit(X)
-cluster = Gausienne.predict(X)
-df1.plot.scatter(x=0, y=1, c=cluster, colormap='plasma')
-plt.savefig('./results/etude_prealable/gaussian-1.png')
 
-X = df2.to_numpy()
-Gausienne = GaussianMixture(n_components=1, random_state=0).fit(X)
-cluster = Gausienne.predict(X)
-df2.plot.scatter(x=0, y=1, c=cluster, colormap='plasma')
-plt.savefig('./results/etude_prealable/gaussian-2.png')
+def gaussian(df, i, n_components):
+    X = df.to_numpy()
+    cluster = GaussianMixture(n_components=n_components, random_state=0).fit_predict(X)
+    df.plot.scatter(x=0, y=1, c=cluster, colormap='plasma')
+    plt.savefig('./results/etude_prealable/gaussian-' + str(i) + '.png')
 
-X = df3.to_numpy()
-Gausienne = GaussianMixture(n_components=2, random_state=0).fit(X)
-cluster = Gausienne.predict(X)
-df3.plot.scatter(x=0, y=1, c=cluster, colormap='plasma')
-plt.savefig('./results/etude_prealable/gaussian-3.png')
 
-X = df4.to_numpy()
-Gausienne = GaussianMixture(n_components=7, random_state=0).fit(X)
-cluster = Gausienne.predict(X)
-df4.plot.scatter(x=0, y=1, c=cluster, colormap='plasma')
-plt.savefig('./results/etude_prealable/gaussian-4.png')
+gaussian(df1, 1, n_components=2)
+gaussian(df2, 2, n_components=1)
+gaussian(df3, 3, n_components=2)
+gaussian(df4, 4, n_components=7)
+gaussian(df5, 5, n_components=3)
 
-X = df5.to_numpy()
-Gausienne = GaussianMixture(n_components=3, random_state=0).fit(X)
-cluster = Gausienne.predict(X)
-df5.plot.scatter(x=0, y=1, c=cluster, colormap='plasma')
-plt.savefig('./results/etude_prealable/gaussian-5.png')
+plt.close(fig='all')
 
 
 ### DBScan
 
-X = df1.to_numpy()
-cluster = DBSCAN(eps=10,min_samples=4).fit_predict(X)
-df1.plot.scatter(x=0,y=1,c=cluster,colormap='plasma')
-plt.savefig('./results/etude_prealable/dbscan-1.png')
+def dbscan(df, i, eps, min_samples):
+    X = df.to_numpy()
+    cluster = DBSCAN(eps=eps, min_samples=min_samples).fit_predict(X)
+    df.plot.scatter(x=0, y=1, c=cluster, colormap='plasma')
+    plt.savefig('./results/etude_prealable/dbscan-' + str(i) + '.png')
 
-X = df2.to_numpy()
-cluster = DBSCAN(eps=25,min_samples=4).fit_predict(X)
-df2.plot.scatter(x=0,y=1,c=cluster,colormap='plasma')
-plt.savefig('./results/etude_prealable/dbscan-2.png')
-
-X = df3.to_numpy()
-cluster = DBSCAN(eps=2.7,min_samples=3).fit_predict(X)
-df3.plot.scatter(x=0,y=1,c=cluster,colormap='plasma')
-plt.savefig('./results/etude_prealable/dbscan-3.png')
-
-X = df4.to_numpy()
-cluster = DBSCAN(eps=2,min_samples=4).fit_predict(X)
-df4.plot.scatter(x=0,y=1,c=cluster,colormap='plasma')
-plt.savefig('./results/etude_prealable/dbscan-4.png')
-
-X = df5.to_numpy()
-cluster = DBSCAN(eps=2,min_samples=4).fit_predict(X)
-df5.plot.scatter(x=0,y=1,c=cluster,colormap='plasma')
-plt.savefig('./results/etude_prealable/dbscan-5.png')
+dbscan(df1, 1, 10, 4)
+dbscan(df2, 2, 25, 4)
+dbscan(df3, 3, 2.7, 3)
+dbscan(df4, 4, 2, 4)
+dbscan(df5, 5, 2, 4)
 
 plt.close(fig='all')
 
